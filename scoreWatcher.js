c7a7352dfe53f782342f7e124c805eece77a7959
Cody Hillyard <codyhillyard@gmail.com>
2023-07-12 20:28:48 -0600

    made the updateChat function

--- scoreWatcher.js
+++ scoreWatcher.js
@@ -5,7 +5,7 @@
 const fs = require('fs');
 const _ = require('lodash'); 
 const tmi = require('tmi.js'), 
-  {channel, username, password, filepath } = require('./settings/t-settings.json'); 
+  {channel, username, password, filepath, filepathb } = require('./settings/t-settings.json'); 
 const options = {
     options: {debug: true}, 
     connection : {
@@ -26,21 +26,26 @@ const options = {
   let totalScore = '-- '; 
   let messArray = []; 
 
+  function updateChat(filename){
+    let allFileContents = fs.readFileSync(filename, 'utf-8');
+    allFileContents.split(/\r?\n/).forEach(line =>  {
+      placeText = (place > 3) ? `${place}th` : places[place-1];  
+      messArray = _.split(_.trim(line), ','); 
+      if (typeof messArray[1] !== 'undefined' && messArray[4] != '0'){    
+        totalScore += (place == 0) ? '' : ` * ${placeText} ${messArray[2]} - (${messArray[4]}) points * --`; 
+      }
+      place++;
+  });  
+    client.say(channel, totalScore); 
+    totalScore = ''; 
+    place = 0;
+  }
+
 fs.watchFile(filepath , () => {
-  const allFileContents = fs.readFileSync(filepath, 'utf-8');
-  allFileContents.split(/\r?\n/).forEach(line =>  {
-    console.log(line); 
-    placeText = (place > 3) ? `${place}th` : places[place-1];  
-    messArray = _.split(_.trim(line), ','); 
-    if (typeof messArray[1] !== 'undefined'){    
-      totalScore += (place == 0) ? '' : ` * ${placeText} ${messArray[2]} - (${messArray[4]}) points * --`; 
-    }
-    place++;
-  });
-  
-  client.say(channel, totalScore); 
-   console.log(totalScore); 
-   totalScore = ''; 
-   place = 0; 
+  updateChat(filepath); 
 });
  
+fs.watchFile(filepathb , () => {
+  updateChat(filepathb); 
+});
+
